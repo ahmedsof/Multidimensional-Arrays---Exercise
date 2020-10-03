292a6c407eea6679673273db44082beb04e32bbe
Ahmed Softev <ahmed_v@abv.bg>
2020-10-03 22:50:38 +0300

    c#

---
+++ 1. Diagonal Difference/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n,n];
+
+            FillMatrix(matrix);
+            int sumD1 = 0;
+            int sumD2 = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    int number = matrix[row, col];
+                    if (row == col)
+                    {
+                        sumD1 += number;
+
+                    }
+
+                    if (col == n - 1 - row)
+                    {
+                        sumD2 += number;
+
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sumD1 - sumD2));
+        }
+
+        private static void FillMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currentRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
