3fdf30aba6e495d49d828333c36abcf4163364f4
Ahmed Softev <ahmed_v@abv.bg>
2020-10-04 00:42:34 +0300

    c#

---
+++ 4. Matrix Shuffling/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Matrix_Shuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            string[,] matrix = new string[rows, cols];
+
+            FillMatrix(matrix);
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "END")
+                {
+                    break;
+                }
+
+                if (!ValidateCommand(command, rows, cols))
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                string[] commands = command.Split();
+                int rowIndexFirst = int.Parse(commands[1]);
+                int colIndexFirst = int.Parse(commands[2]);
+                int rowIndexSecond = int.Parse(commands[3]);
+                int colIndexSecond = int.Parse(commands[4]);
+
+                string temp = String.Empty;
+
+                temp = matrix[rowIndexFirst, colIndexFirst];
+                matrix[rowIndexFirst, colIndexFirst] = matrix[rowIndexSecond, colIndexSecond];
+                matrix[rowIndexSecond, colIndexSecond] = temp;
+
+
+                PrintMatrix(matrix);
+            }
+        }
+
+        private static bool ValidateCommand(string command, int rows, int cols)
+        {
+            bool isValid = false;
+            string[] commands = command.Split();
+            if (commands.Length == 5
+                && commands[0] == "swap"
+                && int.Parse(commands[1]) <= rows
+                && int.Parse(commands[2]) <= cols
+                && int.Parse(commands[3]) <= rows
+                && int.Parse(commands[4]) <= cols)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static void FillMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] currentRow = Console.ReadLine()
+                    .Split().ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+        }
+
+        private static void PrintMatrix(string[,] matrix)
+
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
